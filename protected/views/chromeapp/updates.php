0d6117bbd5e1fd3938ba898363e1f222fb1f0480
bevin <bevin1984@gmail.com>
2011-06-07 14:40:59 +0800

    update

--- protected/views/chromeapp/updates.php
+++ protected/views/chromeapp/updates.php
@@ -1,6 +1,6 @@
 <?php echo '<' . "?xml version='1.0' encoding='UTF-8'?" . '>';?>
 <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
 	<app appid='ngdecanfaaglhkalfinmfeipfmbfajmc'>
-		<updatecheck codebase='http://s1.52wm.com/chromeapp/chrome0_4.crx' version='0.4' />
+		<updatecheck codebase='http://s1.52wm.com/chromeapp/chrome0_5.crx' version='0.5' />
 	</app>
 </gupdate>
